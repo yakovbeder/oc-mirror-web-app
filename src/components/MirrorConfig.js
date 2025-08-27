d99c92c7c965ebd3ad492e97d537e3d999f7f0d1
yakovbeder <yakovbeder@gmail.com>
2025-08-27 10:47:17 +0000

    Fix UI alignment and version number
    - Make Remove button same height as dropdown boxes (38px) for visual consistency
    - Update version number from v3.0 to v3.1.2 in header
    - Both changes improve visual harmony and accuracy

--- src/components/MirrorConfig.js
+++ src/components/MirrorConfig.js
@@ -987,7 +987,7 @@ const MirrorConfig = () => {
                           
                           <button
                             className="btn btn-sm btn-danger"
-                            style={{ alignSelf: 'flex-end' }}
+                            style={{ alignSelf: 'flex-end', height: '38px' }}
                             onClick={() => removeOperatorPackageChannel(opIndex, pkgIndex, channelIndex)}
                           >
                             Remove
