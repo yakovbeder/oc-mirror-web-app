03892b954dd0886cd52d1cf8a9ffafbb0abfd651
yakovbeder <yakovbeder@gmail.com>
2025-08-27 11:38:17 +0000

    Set Remove button height to 42px for optimal match with dropdown boxes

--- src/components/MirrorConfig.js
+++ src/components/MirrorConfig.js
@@ -987,7 +987,7 @@ const MirrorConfig = () => {
                           
                           <button
                             className="btn btn-sm btn-danger"
-                            style={{ alignSelf: 'flex-end', height: '38px' }}
+                            style={{ alignSelf: 'flex-end', height: '42px' }}
                             onClick={() => removeOperatorPackageChannel(opIndex, pkgIndex, channelIndex)}
                           >
                             Remove
